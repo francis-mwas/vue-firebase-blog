7f6b8bef5824364c683f0cb8c52ba639b8e89859
francis mwas <mwangif184@gmail.com>
2021-09-07 08:51:19 +0300

    Started working on adding admin previlege

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,9 +1,80 @@
 <template>
-  <div>Admin component</div>
+  <div class="admin">
+    <div class="container">
+      <h2>Administration</h2>
+      <div class="admin-info">
+        <h2>Add Admin</h2>
+        <div class="input">
+          <input
+            placeholder="Enter user email to make them admin"
+            type="text"
+            v-mode="adminEmail"
+          />
+        </div>
+        <span>{{ ths.functionMsg }}</span>
+        <button>Submit</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Admin',
+  components: {},
+  data() {
+    return {
+      adminEmail: '',
+      functionMsg: null,
+    };
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.profile {
+  .container {
+    max-width: 1000px;
+    padding: 60px 25px;
+
+    h2 {
+      text-align: center;
+      margin-bottom: 16px;
+      font-weight: 300;
+      font-size: 32px;
+    }
+    .admin-info {
+      border-radius: 8px;
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+        0 2px 4px -1px rgba(0, 0, 0, 0.06);
+      padding: 32px;
+      background-color: #f1f1f1;
+      display: flex;
+      flex-direction: column;
+      max-width: 600px;
+      margin: 32px auto;
+
+      span {
+        font-size: 14px;
+      }
+      .input {
+        margin: 16px 0;
+      }
+      label {
+        font-size: 14px;
+        display: block;
+        padding-bottom: 6px;
+        height: 50px;
+        @media (min-width: 900px) {
+        }
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+    button {
+      align-self: center;
+    }
+  }
+}
+</style>
